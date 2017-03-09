e3c2bca81c83be3259bed8f7555bcea039b11406
oeeq <eooaai@gmail.com>
2017-03-09 03:22:51 +0300

    devserver setup

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const process = require('process');
 
 const config = {
   context: path.resolve(__dirname, './src'),
@@ -35,6 +36,8 @@ const config = {
   devtool: 'eval',
   devServer: {
     contentBase: './dist',
+    port: process.env.NODE_PORT || 4000,
+    host: '0.0.0.0',
   },
 };
 
